9831a9c3077a66d51442f5e4c780546680d26118
Yash <proprogramming28@gmail.com>
2024-01-01 17:45:34 +0530

    modify route working successfully...deleteing previous image and adding new one

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,28 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
 const result = require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
 
+const deleteFile = (filePath) => {
+    return new Promise((resolve, reject) => {
+
+        // console.log(filePath);
+        const absolutePath = `E:\\Web-Development\\NodeJs\\UserAdmin App\\${filePath}`;
+
+        fs.unlink(absolutePath, (err) => {
+            if (err) {
+                console.error(`Error deleting file: ${filePath}`);
+                console.error(err);
+                reject(err);
+                return;
+            }
+
+            // console.log(`File deleted successfully: ${filePath}`);
+            resolve();
+        });
+    });
+};
 
 
 //@desc register a user
@@ -136,7 +157,7 @@ const loginUser =asyncHandler( async (req, res) => {
             email:user.email,
             id:user._id,
             phone:user.phone,
-        }},process.env.JWT_SECRET,{expiresIn:"1m"});
+        }},process.env.JWT_SECRET,{expiresIn:"15m"});
         res.status(200).json({"accessToken":jwtToken}); 
     }
     else{
@@ -151,9 +172,47 @@ const loginUser =asyncHandler( async (req, res) => {
 //@desc modify a user's details
 //@route get /api/users/signup
 //@acess private..as only logged in users can modify their details (picture,name)
-const modifyUser = async (req,res) => {
-    res.send("this is modify page");
-}
+const modifyUser = asyncHandler(async (req,res) => {
+    // this will first decode the jwt token and get the email id or phone number of the user.
+    // then based on it, it will search for the user in the databse
+    // then it will modify the name and image according to request
+    // for name it can directly modify, but for file, it needs to write path to the new file in the db and delete the earlier file
+    let email = req.user.email;
+    let phone = req.user.phone;
+    let user;
+    
+    if (email || phone) {
+        // search by either email or phone
+        if(email)
+        {
+            user = await User.findOneAndUpdate({ email :email},{
+                name:req.body.name,
+                imageName:req.file.filename,
+                imagePath:req.file.path,
+            },{new:false});
+           await deleteFile(user.imagePath);
+            
+        }
+        else{
+            user = await User.findOneAndUpdate({ phone :phone},{
+                name:req.body.name,
+                imageName:req.file.filename,
+                imagePath:req.file.path,
+            },{new:false});
+            await deleteFile(user.imagePath);
+        }
+        res.send("user modified");
+    }
+    // if(req.file)
+    // {
+    //     console.log(true);
+    // }
+    // else{
+    //     console.log(false);
+
+    // }
+
+})
 
 //@desc delete a user
 //@route get /api/users/delete
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { signupUser, loginUser, modifyUser, deleteUser } = require('../controllers/userController');
 const { upload } = require("../middleware/imageUploads");
 const validateUser = require("../middleware/UserValidation");
+const validateToken = require("../middleware/validateTokenHandler");
 const router = express.Router();
 const multer = require("multer");
 
@@ -23,7 +24,26 @@ router.post("/signup", (req, res, next) => {
 });
 
 router.post("/login", loginUser);
-router.put("/modify", modifyUser);
+
+
+// router.put("/modify",validateToken ,modifyUser);
+router.put("/modify",validateToken,async (req, res, next) => { 
+  return new Promise((resolve, reject) => {
+    upload.single("image")(req, res, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  })
+    .then(() => modifyUser(req, res))
+    .catch((error) => {
+    //   console.log(error); //? uncomment if you wanna see the error
+    });
+});
+
+
 router.delete("/delete", deleteUser);
 
 module.exports = router;
